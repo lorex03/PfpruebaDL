d79295c9eeaa053f99eb72ecf360f14b5c1a5d27
lorex03 <lorenaflorio03@gmail.com>
2023-10-15 06:44:36 -0300

    falataba la / en el origin primero que se define en el back , en el mismo app

--- Server/src/app.js
+++ Server/src/app.js
@@ -21,7 +21,7 @@ const storage = multer.memoryStorage(); // Almacenamiento en memoria (puedes cam
 
 const server = express();
 const corsOptions = {
-    origin: 'https://pfprueba-dl-bkr9.vercel.app/',
+    origin: 'https://pfprueba-dl-bkr9.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
  preflightContinue: false,
     optionsSuccessStatus: 204
