6a2a27514588db4f4047ab7afabb4dff658e3b94
lorex03 <lorenaflorio03@gmail.com>
2023-10-15 06:41:30 -0300

    saque la / en el app.js del server asi coinciden con el front

--- Server/src/app.js
+++ Server/src/app.js
@@ -28,7 +28,7 @@ const corsOptions = {
 };
 
 server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://pfprueba-dl-bkr9.vercel.app/");
+	res.header("Access-Control-Allow-Origin", "https://pfprueba-dl-bkr9.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
